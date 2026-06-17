f09515eb349302fb63d790cc8ab6fcf18bfa44e8
pratiksonaniya2-del <pratiksonaniya2@gmail.com>
2026-06-17 08:43:10 +0530

    sponsershp added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Achievements from '@/components/Achievements'
-
+import SponsorsPage from '@/components/SponsorShips'
+import Footer from '@/components/Footer'
 export default function Home() {
   return (
     <main>
       <Achievements />
+      <SponsorsPage />
+      <Footer />
     </main>
   )
 }
